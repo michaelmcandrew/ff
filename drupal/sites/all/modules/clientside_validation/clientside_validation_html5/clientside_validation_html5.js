e506f9952d3f731f51198f72743490eb3ce9d48a
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-04-24 12:13:38 +0100

    adding client side validation modules

---
+++ drupal/sites/all/modules/clientside_validation/clientside_validation_html5/clientside_validation_html5.js
@@ -0,0 +1,77 @@
+(function ($) {
+  Drupal.behaviors.clientsideValidationHtml5 = {
+    attach: function (context) {
+      $(document).bind('clientsideValidationAddCustomRules', function(event){
+        function _getMultiplier(a, b, c) {
+          var inta = Number(parseInt(a));
+          var mula = a.length - inta.toString().length - 1;
+
+          var intb = parseInt(b);
+          var mulb = b.toString().length - intb.toString().length - 1;
+          
+          var intc = parseInt(c);
+          var mulc = c.toString().length - intc.toString().length - 1;
+
+          var multiplier = Math.pow(10, Math.max(c, Math.max(mula, mulb)));
+          return (multiplier > 1) ? multiplier : 1;
+        }
+        jQuery.validator.addMethod("Html5Min", function(value, element, param) {
+          //param[0] = min, param[1] = step;
+          var min = param[0];
+          var step = param[1];
+          var multiplier = _getMultiplier(value, min, step);
+
+          value = parseInt(parseFloat(value) * multiplier);
+          min = parseInt(parseFloat(min) * multiplier);
+
+          var mismatch = 0;
+          if (param[1] != 'any') {
+            var step = parseInt(parseFloat(param[1]) * multiplier);
+            mismatch = (value - min) % step
+          }
+          return this.optional(element) || (mismatch == 0 && value >= min);
+        }, jQuery.format('Value must be greater than {0} with steps of {1}.'));
+
+        jQuery.validator.addMethod("Html5Max", function(value, element, param) {
+          //param[0] = max, param[1] = step;
+          var max = param[0];
+          var step = param[1];
+          var multiplier = _getMultiplier(value, max, step);
+
+          value = parseInt(parseFloat(value) * multiplier);
+          max = parseInt(parseFloat(max) * multiplier);
+
+          var mismatch = 0;
+          if (param[1] != 'any') {
+            var step = parseInt(parseFloat(param[1]) * multiplier);
+            mismatch = (max - value) % step
+          }
+          return this.optional(element) || (mismatch == 0 && value <= max);
+        }, jQuery.format('Value must be smaller than {0} and must be dividable by {1}.'));
+
+        jQuery.validator.addMethod("Html5Range", function(value, element, param) {
+          //param[0] = min, param[1] = max, param[2] = step;
+          var min = param[0];
+          var max = param[1];
+          var step = param[2]
+          var multiplier = _getMultiplier(value, min, step);
+
+          value = parseInt(parseFloat(value) * multiplier);
+          min = parseInt(parseFloat(min) * multiplier);
+          max = parseInt(parseFloat(max) * multiplier);
+
+          var mismatch = 0;
+          if (param[2] != 'any') {
+            var step = parseInt(parseFloat(param[2]) * multiplier);
+            mismatch = (value - min) % step
+          }
+          return this.optional(element) || (mismatch == 0 && value >= min && value <= max);
+        }, jQuery.format('Value must be greater than {0} with steps of {2} and smaller than {1}.'));
+
+        jQuery.validator.addMethod("Html5Color", function(value, element, param) {
+          return /^#([a-f]|[A-F]|[0-9]){6}$/.test(value);
+        }, jQuery.format('Value must be a valid color code'));
+      });
+    }
+  }
+})(jQuery);
