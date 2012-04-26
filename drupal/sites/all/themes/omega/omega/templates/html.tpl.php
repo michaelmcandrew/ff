483d68dc0c9387a921ef0b5cb0058da032084017
KatyMoon <katyjockelson@thirdsectordesign.org>
2012-04-26 14:20:42 +0100

    removing javascript from html.tpl.php

--- drupal/sites/all/themes/omega/omega/templates/html.tpl.php
+++ drupal/sites/all/themes/omega/omega/templates/html.tpl.php
@@ -7,7 +7,7 @@
   <?php print $scripts; ?>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
-<body<?php print $attributes;?> onLoad="javascript:custom.init();">
+<body<?php print $attributes;?>>
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
