c3268858e88c882466413b75ea95682ea67e61a7
Peter McAndrew <petermcandrew@thirsectordesign.org>
2012-04-27 20:44:28 +0100

    adding populate fields function and event listener

--- drupal/sites/all/modules/custom/fffb/fb-load-js-sdk.js
+++ drupal/sites/all/modules/custom/fffb/fb-load-js-sdk.js
@@ -6,6 +6,25 @@ window.fbAsyncInit = function() {
 		xfbml      : true,
 		oauth      : true,
 	});
+	FB.Event.subscribe('auth.statusChange', function(response) {
+            if (response.authResponse) {
+              // user has auth'd your app and is logged into Facebook
+              FB.api('/me', function(me){
+                populateFields(me);
+                console.log("me");
+                console.log(me);
+                if (me.name) {
+                  document.getElementById('auth-displayname').innerHTML = me.name;
+                }
+              })
+              document.getElementById('auth-loggedout').style.display = 'none';
+              document.getElementById('auth-loggedin').style.display = 'block';
+            } else {
+              // user has not auth'd your app, or is not logged into Facebook
+              document.getElementById('auth-loggedout').style.display = 'block';
+              document.getElementById('auth-loggedin').style.display = 'none';
+            }
+          });
 };
 (function(d){
 	var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
@@ -14,3 +33,8 @@ window.fbAsyncInit = function() {
 	d.getElementsByTagName('head')[0].appendChild(js);
 }(document));
 
+function populateFields(u){ jQuery('#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-basic-info-civicrm-1-contact-1-contact-first-name').val(u.first_name); 
+jQuery('#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-basic-info-civicrm-1-contact-1-contact-last-name').val(u.last_name);   
+jQuery('#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-basic-info-civicrm-1-contact-1-email-email').val(u.email); 
+
+}
