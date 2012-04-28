b961d8bfa3c6265f0f19501de590da54affbfbea
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-04-28 23:58:28 +0100

    facebook ready for testing - please feed back

--- drupal/sites/all/modules/custom/fffb/fffb-facebook-block.tpl.php
+++ drupal/sites/all/modules/custom/fffb/fffb-facebook-block.tpl.php
@@ -1,10 +1,9 @@
 <div id="fb-root"></div>
-      <div id="auth-status">
-        <div id="auth-loggedout">
-          <a href="#" id="auth-loginlink">Login</a>
+	<div id="auth-status">
+		<div id="auth-loggedout">Help us keep in touch: <a href="#" id="auth-loginlink" >Log in with Facebook</a>
         </div>
         <div id="auth-loggedin" style="display:none">
           Hi, <span id="auth-displayname"></span>  
-        (<a href="#" id="auth-logoutlink">logout</a>)
+        (<a href="#" id="auth-logoutlink">Log out of Facebook</a>)
       </div>
     </div>
--- drupal/sites/all/modules/custom/fffb/fffb.js
+++ drupal/sites/all/modules/custom/fffb/fffb.js
@@ -1,3 +1,11 @@
+(function(d){
+	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement('script'); js.id = id; js.async = true;
+	js.src = "//connect.facebook.net/en_US/all.js";
+	ref.parentNode.insertBefore(js, ref);
+}(document));
+
 window.fbAsyncInit = function() {
 	FB.init({
 		appId      : '434425766583934',
@@ -8,35 +16,45 @@ window.fbAsyncInit = function() {
 	});
 
 	FB.Event.subscribe('auth.statusChange', function(response) {
-            if (response.authResponse) {
-              // user has auth'd your app and is logged into Facebook
-              FB.api('/me', function(me){
-                populateFields(me);
-                console.log("me");
-                console.log(me);
-                if (me.name) {
-                  document.getElementById('auth-displayname').innerHTML = me.name;
-                }
-              })
-              document.getElementById('auth-loggedout').style.display = 'none';
-              document.getElementById('auth-loggedin').style.display = 'block';
-            } else {
-              // user has not auth'd your app, or is not logged into Facebook
-              document.getElementById('auth-loggedout').style.display = 'block';
-              document.getElementById('auth-loggedin').style.display = 'none';
-            }
-          });
-};
-(function(d){
-	var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-	js = d.createElement('script'); js.id = id; js.async = true;
-	js.src = "//connect.facebook.net/en_US/all.js";
-	d.getElementsByTagName('head')[0].appendChild(js);
-}(document));
+	if (response.authResponse) {
+		// user has auth'd your app and is logged into Facebook
+		FB.api('/me', function(me){
+			populateFields(me);
+			if (me.name) {
+				document.getElementById('auth-displayname').innerHTML = me.name;
+			}
+			})
+			document.getElementById('auth-loggedout').style.display = 'none';
+			document.getElementById('auth-loggedin').style.display = 'block';
+		} else {
+			// user has not auth'd your app, or is not logged into Facebook
+			document.getElementById('auth-loggedout').style.display = 'block';
+			document.getElementById('auth-loggedin').style.display = 'none';
+		}
+	});
 
-window.FB.api('/me', function(me){
-	jQuery('#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-basic-info-civicrm-1-contact-1-contact-first-name').val(me.first_name);
-	jQuery('#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-basic-info-civicrm-1-contact-1-contact-last-name').val(me.first_name);
-	jQuery('#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-basic-info-civicrm-1-contact-1-email-email').val(me.first_name);
-})
+	document.getElementById('auth-loginlink').addEventListener('click', function(){
+		FB.login(function(response) {
+   			// handle the response
+ 		}, {scope: 'email, user_education_history, user_work_history'});
+
+	});
+	document.getElementById('auth-logoutlink').addEventListener('click', function(){
+		FB.logout();
+		document.getElementById('auth-loggedout').style.display = 'block';
+		document.getElementById('auth-loggedin').style.display = 'none';
+	})
+	document.getElementById('edit-submit').addEventListener('click', function(){
+		FB.logout();
+	});
+};
+
+function populateFields(me){
+	window.FB.api('/me', function(me){
+		jQuery('#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-basic-info-civicrm-1-contact-1-contact-first-name').val(me.first_name);
+		jQuery('#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-basic-info-civicrm-1-contact-1-contact-last-name').val(me.last_name);
+		jQuery('#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-basic-info-civicrm-1-contact-1-email-email').val(me.email);
+		jQuery('#edit-submitted-civicrm-1-contact-1-fieldset-fieldset-basic-info-civicrm-1-contact-1-cg10-custom-25').val(me.id);
+	})
+}
 
