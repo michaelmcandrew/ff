77262721a52900c97f98d4ae64776ab0e03768a7
KatyMoon <katyjockelson@thirdsectordesign.org>
2012-05-01 15:45:09 +0100

    styling fb link and changes on snagging list

--- drupal/sites/all/modules/custom/fffb/fffb-facebook-block.tpl.php
+++ drupal/sites/all/modules/custom/fffb/fffb-facebook-block.tpl.php
@@ -1,9 +1,8 @@
 <div id="fb-root"></div>
 	<div id="auth-status">
-		<div id="auth-loggedout">Help us keep in touch: <a href="#" id="auth-loginlink" >Log in with Facebook</a>
+		<div id="auth-loggedout"><a href="#" id="auth-loginlink" ><Log in with Facebook><img src="/sites/all/themes/futurefirst/images/facebook.png" width="181" height="32" alt="Log in with Facebook" /></a>
         </div>
         <div id="auth-loggedin" style="display:none">
-          Hi, <span id="auth-displayname"></span>  
-        (<a href="#" id="auth-logoutlink">Log out of Facebook</a>)
+          Hi, <span id="auth-displayname"></span> (<a href="#" id="auth-logoutlink">Log out of Facebook</a>)
       </div>
     </div>
