c409d3d52cb43f409fcd858687d6ab8cec9bab65
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-05-31 02:05:09 +0100

    big commit: sign out users on sign up forms; create a new page teachers/student-signup-page that explains how students sign up; create a new teacher view students permission

---
+++ drupal/sites/all/modules/custom/teachers_view/teachers-view-student-signup-page.tpl.php
@@ -0,0 +1,2 @@
+<p>The signup link that students should use to sign up is: <?php $school ?>http://<?php echo $_SERVER['SERVER_NAME'] . '/' . $signup_link ?></p>
+<p><a href="/<?php echo $signup_link ?>">Click here</a> to log out and visit your signup page (you can't the student sign up page when you are logged in).</p>
