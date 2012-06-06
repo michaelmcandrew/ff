a6055b5245948a21a308fe060235eb70f24e7bbf
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-06-06 19:07:04 +0100

    nicer formating for text on the student sign up page explanation

--- drupal/sites/all/modules/custom/teachers_view/teachers-view-student-signup-page.tpl.php
+++ drupal/sites/all/modules/custom/teachers_view/teachers-view-student-signup-page.tpl.php
@@ -1,2 +1,3 @@
-<p>The signup link that students should use to sign up is: <?php $school ?>http://<?php echo $_SERVER['SERVER_NAME'] . '/' . $signup_link ?></p>
-<p><a href="/<?php echo $signup_link ?>">Click here</a> to log out and visit your signup page (you can't the student sign up page when you are logged in).</p>
+<p>The signup link that students should use to sign up is:</p>
+<ul><li><?php $school ?>http://<?php echo $_SERVER['SERVER_NAME'] . '/' . $signup_link ?></li></ul>
+<p>You can't view that page while you are logged in to this site but you can <a href="/<?php echo $signup_link ?>">click here to log out and visit your school's signup page</a>.</p>
