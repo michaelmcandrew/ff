4fe55e2854a00a92bc3107f9fcc60f166390d132
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-06-11 14:47:49 +0100

    changing text for student sign up search based on Martha's feedback

--- drupal/sites/all/modules/custom/student_form/student-form-list.tpl.php
+++ drupal/sites/all/modules/custom/student_form/student-form-list.tpl.php
@@ -1,5 +1,8 @@
+
+
+
 <p>To sign up to your school's Future First Network, start typing the name of your school below.</p>
-<p>Once you've found your school, hit the 'sign up' button and you'll be taken to your school's sign up page where you can fill in your contact details and join your alumni network.</p>
+<p>Select your school from the dropdown list that will appear and then click the 'sign up' button. You'll then be taken to your school's sign up page where you can fill in your contact details and join your alumni network.</p>
 <p>If you can't find your school or would like any assistance signing up then give the Future First team a call on 020 7239 8933 or email <a href="mailto:networks@futurefirst.org.uk">networks@futurefirst.org.uk</a></p>
 <input id='student_form_list_autocomplete' size='45'>
 <input type='button' id='student_form_list_go' value='sign up'>
