25a38c5acb8c6064678c8afbaf63013922c2d2a5
Peter McAndrew <petermcandrew@thirsectordesign.org>
2012-08-29 11:40:31 +0100

    fixing some jQuery to hide fields when they are not inside a fieldset

--- drupal/sites/all/modules/custom/student_form/js/webform-hide-fields-node-5.js
+++ drupal/sites/all/modules/custom/student_form/js/webform-hide-fields-node-5.js
@@ -2,5 +2,5 @@ jQuery(document).ready(function() {
     //hide facebook
     jQuery('#webform-component-civicrm-1-contact-1-fieldset-fieldset--civicrm-1-contact-1-cg10-custom-25').hide();
     //hide school existing contact field
-    jQuery('#webform-component-civicrm-2-contact-1-fieldset-fieldset--civicrm-2-contact-1-contact-existing').hide();
+    jQuery('#webform-component-civicrm-2-contact-1-contact-existing').hide();
 });
