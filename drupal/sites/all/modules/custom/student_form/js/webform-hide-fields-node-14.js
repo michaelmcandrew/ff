eb716d9a849bf5e1b34ad4965fc80eab39307edb
Peter McAndrew <petermcandrew@thirsectordesign.org>
2012-08-29 11:57:25 +0100

    adding code to hide FB ID field in its new postion (out of fieldset) on webforms

--- drupal/sites/all/modules/custom/student_form/js/webform-hide-fields-node-14.js
+++ drupal/sites/all/modules/custom/student_form/js/webform-hide-fields-node-14.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
     //hide facebook
-    jQuery('#webform-component-civicrm-1-contact-1-fieldset-fieldset--civicrm-1-contact-1-cg10-custom-25').hide();
+    jQuery('#webform-component-civicrm-1-contact-1-cg10-custom-25').hide();
     //hide school existing contact field
     jQuery('#webform-component-civicrm-5-contact-1-contact-existing').hide();
 });
