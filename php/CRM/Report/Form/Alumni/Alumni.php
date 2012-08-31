ee51a25761f79c9c3f357157e5049aa7e49014d6
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-08-31 13:12:30 +0000

    summary report getting better - some columns and some ajax editing

--- php/CRM/Report/Form/Alumni/Alumni.php
+++ php/CRM/Report/Form/Alumni/Alumni.php
@@ -3,6 +3,14 @@ require_once 'CRM/Report/Form.php';
 require_once 'CRM/Core/BAO/CustomOption.php';
 class CRM_Report_Form_Alumni_Alumni extends CRM_Report_Form {
 
+    protected $_exposeContactID = false;
+
+    protected $_instanceButtonName = 'test';
+    protected $_createNewButtonName = 'test';
+    protected $_printButtonName    = 'test';
+    protected $_pdfButtonName      = 'test';
+    protected $_add2groupSupported = false;
+
     function getCustomDataOptions($id){
         $options=CRM_Core_BAO_CustomOption::getCustomOption($id);
         $return[null]='- select -';
@@ -15,23 +23,25 @@ class CRM_Report_Form_Alumni_Alumni extends CRM_Report_Form {
     function __construct() {
         
         $this->_columns = array(
-            'student' => array(
+            'alumni' => array(
                 'dao' => 'CRM_Contact_DAO_Contact',
+                'alias' => 'alumni',
                 'fields' => array(
                     'id' => array(
-                        'title' => 'id',
-                        'no_display' => true
+                        'no_display'=> true, 
+                        'required'  => true,
                     ),
-                    'name' => array(
+                    'display_name' => array(
                         'title' => 'Name',
-                        'no_display' => true
+                        'dbAlias' => 'display_name',
+                        'required' => true
                     )
                 ),
                 'filters' => array(
                     'name' => array(
                         'title' => 'Name',
                         'operatorType' => CRM_Report_Form::OP_STRING,
-                        'options' => range('1900','2012')
+                        'options' => range('2012', '1950')
                     ),                    
                     'year' => array(
                         'title' => 'Year',
@@ -94,23 +104,27 @@ class CRM_Report_Form_Alumni_Alumni extends CRM_Report_Form {
                         'options' => range('1900','2012')
                     ),                    
                                        
-                )
+                ),
+                
             ),
             'email' => array(
+                'alias' => 'email',
                 'dao' => 'CRM_Core_DAO_Email',
                 'fields' => array(
-                    'mobile' => array(
-                        'title' => 'Mobile',
-                        'no_display' => true
+                    'email' => array(
+                        'title' => 'Email',
+                        'required' => true
                     )
                 )
             ),
             'phone' => array(
+                'alias' => 'phone',
                 'dao' => 'CRM_Core_DAO_Phone',
                 'fields' => array(
                     'mobile' => array(
+                        'dbAlias' => 'phone',
                         'title' => 'Mobile',
-                        'no_display' => true
+                        'required' => true
                     )
                 )
             )
@@ -118,14 +132,72 @@ class CRM_Report_Form_Alumni_Alumni extends CRM_Report_Form {
 
         parent::__construct();
     }
+
+    function preProcess() {
     
-    function select(){
-        $this->_select = 'SELECT *';
+      parent::preProcess();
     }
 
-    function from(){
-        $this->_from = "FROM civicrm_contact{$this->_aclFrom}";        
+    function postProcess( ) {
+        
+        $this->beginPostProcess( );
+        
+        $this->buildACLClause( $this->_aliases['alumni'] );
+
+        $sql = $this->buildQuery( true );
+//        
+        $rows = array();
+
+        $this->buildRows ( $sql, $rows );
+        
+        $this->formatDisplay( $rows );
+        
+        
+        $this->doTemplateAssignment( $rows );
+        $this->endPostProcess( );
     }
+
+    
+    // function select(){
+    //     $this->_select = 'SELECT *';
+    // }
+
+    function from() {
+
+      $this->_from = " 
+        FROM civicrm_contact AS {$this->_aliases['alumni']} 
+        LEFT JOIN civicrm_email AS {$this->_aliases['email']} ON {$this->_aliases['email']}.contact_id={$this->_aliases['alumni']}.id 
+        LEFT JOIN civicrm_phone AS {$this->_aliases['phone']} ON {$this->_aliases['phone']}.contact_id={$this->_aliases['alumni']}.id AND phone_type_id=2
+        {$this->_aclFrom}
+      "; 
+    }
+
+    function where(){
+        $this->_where = "WHERE {$this->_aclWhere}";        
+    }
+
+    function groupBy(){
+        $this->_groupBy = "GROUP BY {$this->_aliases['alumni']}.id";        
+    }
+
+    function orderBy(){
+        $this->_orderBy = "ORDER BY last_name";        
+    }
+    
+    
+    function modifyColumnHeaders(){
+        $this->_columnHeaders['actions']=array('title'=>'Actions');
+        
+    }
+    
+    function alterDisplay( &$rows ) {
+        foreach($rows as &$row){
+            $row['actions']="
+                <a href='#' onclick='updateViaProfile({$row['alumni_id']}); return false;'>e</a>
+                <a href='/civicrm/report/school-dashboard/alumni-detail?id={$row['alumni_id']}'>v</a>";
+        }
+    }
+    
 }
 
 ?>
--- templates/CRM/Report/Form/Alumni/Alumni.tpl
+++ templates/CRM/Report/Form/Alumni/Alumni.tpl
@@ -1,1 +1,25 @@
 {include file="CRM/Report/Form.tpl"}
+<div id="update_via_profile"></div>
+<script type="text/javascript">
+{literal}
+function updateViaProfile( contactID ) {
+
+    var dataURL = {/literal}"{crmURL p="civicrm/profile/edit" q="reset=1&snippet=5&context=dialog&blockNo=1&gid=14&id=" h=0}"{literal};
+    dataURL = dataURL + contactID;
+
+    cj.ajax({
+        url: dataURL,
+        success: function( content ) {
+            cj( '#update_via_profile' ).show( ).html( content ).dialog({
+                title: "Update contact details",
+                modal: true,
+                
+                close: function(event, ui) {
+                    cj('#update_via_profile').fadeOut(5000);
+                }
+            });
+        }
+    });
+}
+{/literal}
+</script>
