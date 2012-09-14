6a57776282540899b1826ec5b5789b09bf30d03c
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-09-14 09:09:24 +0000

    styling changes

--- drupal/sites/all/modules/custom/school_dashboard/js/recent-leavers.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function() {
-    jQuery('#recent_leavers').load('school-dashboard/report?force=1&recent=1&snippet=1&section=2');
-});
---
+++ drupal/sites/all/modules/custom/school_dashboard/js/recent-sign-ups.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function() {
+    jQuery('#recent_sign_ups').load('school-dashboard/report?force=1&recent=1&snippet=1&section=2');
+});
