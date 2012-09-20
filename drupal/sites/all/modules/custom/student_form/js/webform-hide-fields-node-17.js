e45e03bb4dbd8d834ccdee8920a7ddbc29b2692f
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-09-20 19:50:30 +0100

    hide tracking fields

--- drupal/sites/all/modules/custom/student_form/js/webform-hide-fields-node-17.js
+++ drupal/sites/all/modules/custom/student_form/js/webform-hide-fields-node-17.js
@@ -3,4 +3,6 @@ jQuery(document).ready(function() {
     jQuery('#webform-component-civicrm-1-contact-1-cg10-custom-25').hide();
     //hide school existing contact field
     jQuery('#webform-component-civicrm-2-contact-1-contact-existing').hide();
+    //hide tracking field
+    jQuery('#webform-component-civicrm-1-contact-1-cg14-custom-61').hide();
 });
