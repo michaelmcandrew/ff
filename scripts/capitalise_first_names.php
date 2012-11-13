e2e5a2643f648778c504ec2951bace82bd8c6f98
Thom Wilhelm <thom@futurefirst.org.uk>
2012-11-13 17:19:20 +0000

    Adding script which will capitalise first name

---
+++ scripts/capitalise_first_names.php
@@ -0,0 +1,54 @@
+<?php
+require_once '../drupal/sites/all/modules/civicrm/civicrm.config.php' ;
+require_once  'CRM/Core/Config.php';
+
+$rowCount = 100;
+$totalProcessed = 0;
+$totalUpdated = 0;
+
+$param = array(
+	"version" => 3,
+	"option.limit" => $rowCount,
+	"option.offset" => 0,
+);
+
+$result = civicrm_api("Contact", "Get", $param);
+
+while (
+	is_array($result) &&
+	$result["count"] > 0
+) {
+	echo "Processing " . $result["count"] . " rows... ".$param["option.offset"]." completed\n";
+
+	$value = $result["values"];
+	
+	foreach($value as $contact_id => $contact) {
+		
+		$newFirstName = ucfirst($contact["first_name"]);
+		
+		if($newFirstName != $contact["first_name"]) {
+			
+			$updateParam = array(
+				"version" => 3,
+				"id" => $contact_id,
+				"first_name" => $newFirstName,
+			);
+			
+			civicrm_api("Contact", "update", $updateParam);
+			
+			$totalUpdated++;
+			
+		}
+		
+		$totalProcessed++;
+		
+	}
+	
+	$param["option.offset"] += $rowCount;
+	$result = civicrm_api("Contact", "Get", $param);
+	
+}
+
+echo "Total rows processed: $totalProcessed, Total updated: $totalUpdated";
+echo "\n";
+?>
