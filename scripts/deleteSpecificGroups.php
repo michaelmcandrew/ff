0a8f9407e77cddf9cc4bbddecd5d5f9ae47b5665
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2012-12-13 11:03:58 +0000

    deleting groups 120 to 160

--- scripts/deleteSpecificGroups.php
+++ scripts/deleteSpecificGroups.php
@@ -3,7 +3,7 @@ require_once '../drupal/sites/all/modules/civicrm/civicrm.config.php' ;
 require_once  'CRM/Core/Config.php';
 CRM_Core_Config::singleton( );
 
-$groupsToDelete = range(69,114);
+$groupsToDelete = range(120,160);
 foreach($groupsToDelete as $group){
   print_r(civicrm_api("Group", "Delete", array('version' => 3, 'id' => $group)));
 }
